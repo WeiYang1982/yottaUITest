83ed72a6bb13adf152bb5fe313b9c6964393190f
A <jianxing12345@sina.com>
2018-02-09 15:04:08 +0800

    增加1秒等待时间

--- src/test/java/com/yottabyte/stepDefs/SetKeyWithValue.java
+++ src/test/java/com/yottabyte/stepDefs/SetKeyWithValue.java
@@ -16,6 +16,11 @@ public class SetKeyWithValue {
     public void iSetTheParameterWithValue1(String  elementName, String value){
         boolean flag = true;
         WebElement element = GetElementFromPage.getWebElementWithName(elementName);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         while (flag){
             element.click();
             element.clear();
