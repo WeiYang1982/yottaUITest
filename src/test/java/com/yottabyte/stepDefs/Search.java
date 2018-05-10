f3f0a8b50c644b57eb452df449f3b0a651aa0479
sunx.xiaojing <sun.xiaojing@yottabyte.cn>
2018-05-10 10:32:26 +0800

    增加知识搜索step

--- src/test/java/com/yottabyte/stepDefs/Search.java
+++ src/test/java/com/yottabyte/stepDefs/Search.java
@@ -18,33 +18,50 @@ public class Search {
     /**
      * 查找搜索内容是否正确
      *
-     * @param columnName 搜索的列
-     * @param keyword    搜索的关键字
+     * @param columnName 下拉搜索对应的列表列
+     * @param keyword    下拉搜索选择的内容
+     * @param searchList 输入搜索文本后的列表
+     * @param searchName 输入搜索的内容
      */
-    @Then("I will see the list of \"([^\"]*)\" contains \"([^\"]*)\"")
-    public void search(String columnName, String keyword) {
+    @Then("I will see the list of \"([^\"]*)\" contains \"([^\"]*)\" or I see the \"([^\"]*)\" contains \"([^\"]*)\"")
+    public void search(String columnName, String keyword, String searchList, String searchName) throws Exception {
         WebDriver webDriver = LoginBeforeAllTests.getWebDriver();
-        // 获取表体
-        WebElement tableBody = GetElementFromPage.getWebElementWithName("getTableBody");
-        // 获取分页
-        List<WebElement> paging = webDriver.findElements(By.className("number"));
-        // 获取表头
-        WebElement tableHeader = webDriver.findElement(By.className("el-table__header"));
-
-        String className = null;
-        // 查找待查找元素在第几列
-        for (WebElement webElement : tableHeader.findElements(By.tagName("th"))) {
-            if (columnName.equals(webElement.getText())) {
-                className = webElement.getAttribute("class").split(" ")[0];
+        // 选择下拉菜单时
+        if ("".equals(searchName) && !"".equals(keyword)) {
+            if ("".equals(columnName)) {
+                throw new Exception("缺少第一个参数！");
             }
-        }
-        for (int i = paging.size(); i >0; i--) {
-            WaitForElement.waitForElementWithExpectedCondition(webDriver, ExpectedConditions.elementToBeClickable(paging.get(i)));
-            paging.get(i).click();
-            for (WebElement element : tableBody.findElements(By.className(className))) {
-                if (!element.getText().contains(keyword)) {
-                    assertTrue(false);
+            // 获取表体
+            WebElement tableBody = GetElementFromPage.getWebElementWithName("getTableBody");
+            // 获取分页
+            List<WebElement> paging = webDriver.findElements(By.className("number"));
+            // 获取表头
+            WebElement tableHeader = webDriver.findElement(By.className("el-table__header"));
+
+            String className = null;
+            // 所要查找元素对应的列
+            for (WebElement webElement : tableHeader.findElements(By.tagName("th"))) {
+                if (columnName.equals(webElement.getText())) {
+                    className = webElement.getAttribute("class").split(" ")[0];
+                    break;
                 }
+            }
+            for (int i = 0; i < paging.size(); i++) {
+                WaitForElement.waitForElementWithExpectedCondition(webDriver, ExpectedConditions.elementToBeClickable(paging.get(i)));
+                paging.get(i).click();
+                for (WebElement element : tableBody.findElements(By.className(className))) {
+                    if (!element.getText().contains(keyword)) {
+                        assertTrue(false);
+                    }
+                }
+            }
+        } else {
+            if("".equals(searchList)){
+                throw new Exception("缺少第三个参数！");
+            }
+            List<WebElement> search = GetElementFromPage.getWebElementsWithName(searchList);
+            for (WebElement listElement : search) {
+                assertTrue(listElement.getText().contains(searchName));
             }
         }
     }
