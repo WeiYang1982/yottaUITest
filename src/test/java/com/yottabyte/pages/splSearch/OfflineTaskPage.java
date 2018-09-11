7b960402471968bd9ddfafe2e4e5b1c30f08c17f
yang.wei <yang.wei@yottabyte.cn>
2018-09-11 14:21:09 +0800

    等待页面加载完毕

--- src/test/java/com/yottabyte/pages/splSearch/OfflineTaskPage.java
+++ src/test/java/com/yottabyte/pages/splSearch/OfflineTaskPage.java
@@ -2,9 +2,13 @@ package com.yottabyte.pages.splSearch;
 
 import com.yottabyte.pages.PageTemplate;
 import com.yottabyte.utils.ElementExist;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -14,14 +18,30 @@ import java.util.List;
 public class OfflineTaskPage extends PageTemplate {
     public OfflineTaskPage(WebDriver driver) {
         super(driver);
+        isLoaded();
     }
 
     @FindBy(className = "el-table_1_column_1")
     private List<WebElement> tdList;
 
+    @FindBy(className = "el-table__empty-text")
+    private WebElement emptyElement;
+
     public WebElement getFirstData() {
-        ElementExist.isElementExist(webDriver,tdList.get(1));
+
+
 //        WaitForElement.waitForElementWithExpectedCondition(webDriver, ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("el-table_1_column_1")));
         return tdList.get(1);
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+        WebDriverWait wait = new WebDriverWait(webDriver, 10, 1000);
+        wait.until(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver driver) {
+                return driver.findElements(By.className("el-table_1_column_1")).size() > 1;
+            }
+        });
     }
 }
